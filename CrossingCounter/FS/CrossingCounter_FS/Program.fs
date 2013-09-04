5b0a801ae894c4d70cce882b93028700a7b62478
Akihiro Takada <umebayashi.akihiro@gmail.com>
2013-09-05 02:23:28 +0900

    Update CrossingCounter_FS

--- CrossingCounter/FS/CrossingCounter_FS/Program.fs
+++ CrossingCounter/FS/CrossingCounter_FS/Program.fs
@@ -1,4 +1,19 @@
-﻿[<EntryPoint>]
-let main argv = 
-    printfn "Hello World!"
-    0 // 整数の終了コードを返します
+﻿open System
+open System.IO
+
+module Program =
+
+    // データファイルを読み込み数値のシーケンスを作成する
+    let readNumbers filePath =
+        File.ReadLines(filePath)
+        |> Seq.cast<string>
+        |> Seq.map (fun s -> Int32.Parse(s))
+
+    [<EntryPoint>]
+    let main argv =
+        let numbers = readNumbers argv.[0]
+
+        for number in numbers do
+            printfn "%d" number
+
+        0 // 整数の終了コードを返します
